1bef9d0a647164418fe762505f4b633a4e3b95d3
Chris Martine <cmmartine@gmail.com>
2024-04-01 09:30:12 -0400

    Change chatroom fetching and connection creation placing calls into UserList

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,32 +1,19 @@
 import consumer from "./consumer";
 
-async function getCurrentUserChatrooms() {
-  const response = await fetch('/users/current_users_chatrooms');
-  const chatrooms = await response.json();
-  return chatrooms
-};
-
-async function setupUserChatroomConsumers() {
-
-  let users_chatrooms = await getCurrentUserChatrooms();
-
-  users_chatrooms.forEach((room) => {
-  consumer.subscriptions.create({ channel: "ChatroomChannel", room: `chatroom_${room.id}` }, {
+export default function setupUserChatroomConsumers(chatroom) {
+  consumer.subscriptions.create({ channel: "ChatroomChannel", room: `chatroom_${chatroom.id}` }, {
     connected() {
       // Called when the subscription is ready for use on the server
-      console.log(`connected to chatroom_${room.id}`)
+      console.log(`connected to chatroom_${chatroom.id}`)
     },
-      
+        
     disconnected() {
       // Called when the subscription has been terminated by the server
     },
-      
+        
     received(data) {
         // Called when there's incoming data on the websocket for this channel
         console.log(data)
     }
   });
-  });
 }
-
-setupUserChatroomConsumers();
--- app/javascript/components/UserList.jsx
+++ app/javascript/components/UserList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { CurrentUserContext } from "./Main";
+import setupUserChatroomConsumers from "../channels/chatroom_channel";
 import { useContext } from "react";
 
 export default function UserList() {
   const [allUsers, setAllUsers] = useState([]);
   const [usersFetched, setUsersFetched] = useState(false);
+  const [chatrooms, setChatrooms] = useState([]);
+  const [chatroomsFetched, setChatroomsFetched] = useState(false);
   const currentUser = useContext(CurrentUserContext);
 
   useEffect(() => {
@@ -13,13 +16,18 @@ export default function UserList() {
       getUsers();
       setUsersFetched(true);
     };
+    if (!chatroomsFetched) {
+      getCurrentUserChatrooms();
+      setChatroomsFetched(true);
+    }
+    openChatroomConnections();
   });
 
   function getUsers() {
     fetch('/users/index')
     .then((res) => res.json())
     .then((data) => {
-      let userArray = []
+      let userArray = [];
       data.map((user) => {
         userArray.push(user);
       });
@@ -27,14 +35,31 @@ export default function UserList() {
     });
   };
 
-  const openChatRoom = (currentUserId, selectedUserId) => {
-    
+  function getCurrentUserChatrooms() {
+    fetch('/users/current_users_chatrooms')
+    .then((res) => res.json())
+    .then((data) => {
+      let roomArray = [];
+      data.map((room) => {
+        roomArray.push(room);
+      });
+      setChatrooms(roomArray);
+    });
+  };
+
+  function openChatroomConnections() {
+    if (chatrooms.length > 0) {
+      chatrooms.forEach((room) => {
+        console.log(room)
+        setupUserChatroomConsumers(room);
+      });
+    }
   };
 
   const makeUserList = () => {
     const userList = allUsers.map((user) => (
       <div id={`user.${user.id}`}>
-      <li key={user.id}>{user.username} {openChatRoom(currentUser.id, user.id)}</li>
+      <li key={user.id}>{user.username}</li>
       </div>
     ))
 
