752cadcbab46cfe52d53193057770874f78d05c7
Chris Martine <cmmartine@gmail.com>
2024-05-30 09:01:50 -0400

    Add message model update_read_status tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Message, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:chatroom) }
   end
+
+  describe '.update_read_status' do
+    it 'changes read_status to true when it was false' do
+      msg = Message.create(body: 'Test', read_status: false, user_id: 1, chatroom_id: 1)
+      msg.update_read_status
+      expect(msg.read_status).to be(true)
+    end
+
+    it 'does not change when read_status is already true' do
+      msg = Message.create(body: 'Test', read_status: true, user_id: 1, chatroom_id: 1)
+      msg.update_read_status
+      expect(msg.read_status).to be(true)
+    end
+  end
 end
