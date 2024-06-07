1e12144dcb0905ec50b8a6b178cefea8a3242d47
Chris Martine <cmmartine@gmail.com>
2024-06-07 08:28:43 -0400

    Add rspecs for chatroom.most_recent_non_current_user_message_status

--- spec/models/chatroom_spec.rb
+++ spec/models/chatroom_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Chatroom, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { should have_many(:messages) }
     it { should have_and_belong_to_many(:users) }
+  end
+
+  describe '.most_recent_non_current_user_message_status' do
+    let!(:chatroom) { Chatroom.create }
+    let!(:current_user) { User.create(username: 'Alfred', email: 'alfred1@chatroom.com', password: 'Alfred1!', password_confirmation: 'Alfred1!') }
+    let!(:non_current_user) { User.create(username: 'Bianca', email: 'bianca1@chatroom.com', password: 'Bianca1!', password_confirmation: 'Bianca1!') }
+
+    it 'returns true when there are no non_current_user messages' do
+      current_user.chatrooms << chatroom
+      non_current_user.chatrooms << chatroom
+      expect(chatroom.most_recent_non_current_user_message_status(current_user.id)).to be(true)
+    end
+
+    it 'returns false when there are is an unread non_current_user message' do
+      current_user.chatrooms << chatroom
+      non_current_user.chatrooms << chatroom
+      Message.create(body: 'Hello', read_status: false, user_id: non_current_user.id, chatroom_id: chatroom.id)
+      expect(chatroom.most_recent_non_current_user_message_status(current_user.id)).to be(false)
+    end
   end
 end
