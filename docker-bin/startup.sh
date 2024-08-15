28c62f37e6d2d15f93d33c0bb46c516222a83254
Chris Martine <cmmartine@gmail.com>
2024-08-15 08:47:14 -0400

    Remove server.pid on container startup

--- docker-bin/startup.sh
+++ docker-bin/startup.sh
@@ -6,4 +6,9 @@ THIS_SCRIPT_DIR=$(dirname $0)
 # Set variables for application
 # source $THIS_SCRIPT_DIR/env.sh
 
+# Remove postgres server file if it exists
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
 exec "$@"
