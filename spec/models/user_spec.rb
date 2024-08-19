7268904df0579af7e58293eaaa52160637929107
Chris Martine <cmmartine@gmail.com>
2024-08-19 14:59:46 -0400

    Add User model tests for search and filter_search_to_name_and_id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,50 @@ RSpec.describe User, type: :model do
       expect(return_id).to be(2)
     end
   end
+
+  describe 'self.search' do
+    before do
+      User.create(username: 'Bianca', email: 'bianca1@chatroom.com', password: 'Bianca1!', password_confirmation: 'Bianca1!')
+      User.create(username: 'Brittney', email: 'brittney1@chatroom.com', password: 'Brittney1!', password_confirmation: 'Brittney1!')
+    end
+
+    search_params_lower_b = 'b'
+
+    search_params_upper_a = 'A'
+
+    it 'returns all users with the letter B in their name when user input is lowercase and username letter is uppercase' do
+      output = User.search(search_params_lower_b)
+      expect(output).to include(User.find_by(username: 'Bianca'))
+      expect(output).to include(User.find_by(username: 'Brittney'))
+    end
+
+    it 'returns all users with the letter A in their name when user input is uppercase and username letter is lowercase' do
+      output = User.search(search_params_upper_a)
+      expect(output).to include(User.find_by(username: 'Bianca'))
+    end
+  end
+
+  describe 'self.filter_search_to_name_and_id' do
+    before do
+      User.create(username: 'Bianca', email: 'bianca1@chatroom.com', password: 'Bianca1!', password_confirmation: 'Bianca1!')
+      User.create(username: 'Brittney', email: 'brittney1@chatroom.com', password: 'Brittney1!', password_confirmation: 'Brittney1!')
+    end
+
+    expected_result = [
+      {
+        username: 'Bianca',
+        id: 1
+      },
+      {
+        username: 'Brittney',
+        id: 2
+      }
+    ]
+
+    it('filters to just name and id') do
+      input = User.where('username LIKE ?', 'B%')
+      output = User.filter_search_to_name_and_id(input)
+      expect(output).to eq(expected_result)
+    end
+  end
 end
