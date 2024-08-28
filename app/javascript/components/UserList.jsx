ffe0bf698bb73c1e98303f60cf1594ae2211c9a5
Chris Martine <cmmartine@gmail.com>
2024-08-28 11:03:30 -0400

    Add UserList Tab switching for buddies and requests

--- app/javascript/components/UserList.jsx
+++ app/javascript/components/UserList.jsx
@@ -9,9 +9,11 @@ import UserSearchBar from "./UserSearchBar";
 import { getUsers } from "../util/userUtil";
 import { LightDarkContext } from "./Main";
 import { THEMES } from "../constants/themes";
+import { LIST_TYPES } from "../constants/listTypes";
 import setupDraggingAndResizing from "../util/dragAndResize";
 
 export default function UserList(props) {
+  const [listType, setListType] = useState(LIST_TYPES.buddies);
   const [allUsers, setAllUsers] = useState([]);
   const [usersFetched, setUsersFetched] = useState(false);
   const currentUser = useContext(CurrentUserContext);
@@ -27,7 +29,18 @@ export default function UserList(props) {
     };
   });
 
-  const makeUserList = () => {
+  const renderList = () => {
+    switch (listType) {
+      case LIST_TYPES.buddies:
+        return makeBuddyList();
+      case LIST_TYPES.requests:
+        return makeRequestList();
+      default:
+        return <div>Error, please reload the page</div>
+    };
+  };
+
+  const makeBuddyList = () => {
     const userListCss = lightDarkTheme == THEMES.light ? 'userlist' : 'userlist userlist-dark';
     const userList = allUsers.map((user) => {
       if (currentUser.id !== user.id)
@@ -36,6 +49,16 @@ export default function UserList(props) {
           <UserMessageNotification userInfo={user}/>
         </div>
     })
+    return <ul className={userListCss}>{userList}</ul>
+  };
+
+  const makeRequestList = () => {
+    const userListCss = lightDarkTheme == THEMES.light ? 'userlist' : 'userlist userlist-dark';
+    const userList = 
+      <div key={''} className='userlist-components'>
+          
+      </div>
+    
     return <ul className={userListCss}>{userList}</ul>
   };
 
@@ -59,10 +82,10 @@ export default function UserList(props) {
           <div className='userlist-sidebar'>
             <UserSearchBar />
             <div className='userlist-tab-container'>
-              <h4 className='userlist-tab'>Buddies</h4>
-              <h4 className='userlist-tab'>Requests</h4>
+              <h4 className='userlist-tab buddies-tab' onClick={() => setListType(LIST_TYPES.buddies)}>Buddies</h4>
+              <h4 className='userlist-tab requests-tab' onClick={() => setListType(LIST_TYPES.requests)}>Requests</h4>
             </div>
-            {makeUserList()}
+            {renderList()}
             <LightDarkModeBtn changeLightDarkTheme={changeLightDarkTheme}/>
           </div>
         </div>
@@ -84,10 +107,10 @@ export default function UserList(props) {
           <div className='userlist-sidebar userlist-sidebar-dark'>
             <UserSearchBar />
             <div className='userlist-tab-container'>
-              <h4 className='userlist-tab userlist-tab-dark'>Buddies</h4>
-              <h4 className='userlist-tab userlist-tab-dark'>Requests</h4>
+              <h4 className='userlist-tab userlist-tab-dark buddies-tab' onClick={() => setListType(LIST_TYPES.buddies)}>Buddies</h4>
+              <h4 className='userlist-tab userlist-tab-dark requests-tab' onClick={() => setListType(LIST_TYPES.requests)}>Requests</h4>
             </div>
-            {makeUserList()}
+            {renderList()}
             <LightDarkModeBtn changeLightDarkTheme={changeLightDarkTheme}/>
           </div>
         </div>
---
+++ app/javascript/constants/listTypes.js
@@ -0,0 +1,4 @@
+export const LIST_TYPES = {
+  buddies: "buddiesList",
+  requests: "requestsList"
+};
