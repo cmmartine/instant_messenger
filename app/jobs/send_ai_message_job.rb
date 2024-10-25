4702eb2f5a2e413e8185a6fc7f18bea204f0c931
Chris Martine <cmmartine@gmail.com>
2024-10-25 13:57:22 -0400

    Add chatbot unavailable response when chatbot is down

--- app/jobs/send_ai_message_job.rb
+++ app/jobs/send_ai_message_job.rb
@@ -1,5 +1,7 @@
 class SendAiMessageJob < ApplicationJob
   def perform(message)
-    `python3 app/jobs/h2oai_api.py '#{message[:body]}'`
+    ai_response = `python3 app/jobs/h2oai_api.py '#{message[:body]}'`
+    no_ai_response_msg = 'Chatbot is currently unavailable, please try again later.'
+    ai_response == '' ? no_ai_response_msg : ai_response
   end
 end
