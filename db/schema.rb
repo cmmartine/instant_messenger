d23aef3f87df3c8510a04ed554b8e5e05b8285e1
Chris Martine <cmmartine@gmail.com>
2025-05-02 10:46:35 -0400

    Add username index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_23_133705) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_02_133800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_23_133705) do
     t.string "theme", default: "light"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "buddies", "users"
